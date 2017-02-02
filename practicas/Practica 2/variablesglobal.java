857ab16ea307fc0f3cebaab89e456b8ea6d65808
jesus1 <garciajesus96@hotmail.com>
2017-02-02 11:45:03 -0600

    Estructura del repositorio

---
+++ practicas/Practica 2/variablesglobal.java
@@ -0,0 +1,25 @@
+public class variablesglobal{
+
+static byte dos ;
+static short _tre03;
+static int $ocho;
+static long nueve ;
+static double var_double; 
+static char jm;
+static boolean var_boolean;
+static String var_string ;
+
+
+
+public static void main (String []args){
+
+System.out.println("Mi variable de tipo byte vale = "+ dos);
+System.out.println("Mi variable  de tipo short vale= "+ _tre03);
+System.out.println("Mi variable  de tipo  int vale="+ $ocho);
+System.out.println("Mi variable de tipo long vale ="+ nueve+ "L");
+System.out.println("Mi variable de tipo double vale ="+ var_double+ "L");
+System.out.println("Mi variable de tipo caracter vale ="+ jm+"\u0000");
+System.out.println("Mi variable de tipo boleana  imprime = "+ var_boolean);
+System.out.println("mi variable de tipo string es:" + var_string);
+	}
+}
